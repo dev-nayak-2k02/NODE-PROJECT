df59d8656abfb0beaf0ffa1832792d8b2f376d93
dev nayak10 <devprasannanayak@gmail.com>
2025-08-20 21:30:42 +0530

    node.js: js files are added

---
+++ firstDigit.js
@@ -0,0 +1,4 @@
+var a = 123;
+var c = parseInt(a/100);
+console.log('Amount Entered: ', a);
+console.log('First digit is: ', c);
---
+++ secondDigit.js
@@ -0,0 +1,5 @@
+var a = 123;
+var b = parseInt(a/10);
+var c = b%10;
+console.log("Digit Entered: ",a);
+console.log("Second digit is: ",c);
