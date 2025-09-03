f76ace553870a682ecdb501d0a5676b21bdc4880
dev nayak10 <devprasannanayak@gmail.com>
2025-09-03 23:02:26 +0530

    Added content

--- 300825.js
+++ 300825.js
@@ -10,3 +10,17 @@ var reduce = function(nums, fn, init) {
 
 let nums = [1,2,3,4];
 let fn = function sum(accum, curr) { return accum + curr * curr; }
+
+let ans = (function abcd(){
+    let variable = 12;
+    return{
+        Set: function(val){
+            variable = val;
+        },
+        get: function() {
+            console.log(variable);
+        }
+    };
+})();
+ans.Set(38);
+ans.get();
