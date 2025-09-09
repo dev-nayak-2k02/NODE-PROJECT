d3f68394c72aec4678b15e29eb5a05c7a184452b
dev nayak10 <devprasannanayak@gmail.com>
2025-09-09 17:25:31 +0530

    content added

---
+++ 090925node/addition.js
@@ -0,0 +1,9 @@
+function addition(){
+    var a = 50;
+    var b = 70;
+    var s = a+b;
+    console.log('First number: ',a);
+    console.log('Second number: ',b);
+    console.log('Sum is: ',s);
+}
+export default addition;
---
+++ 090925node/index.js
@@ -0,0 +1,2 @@
+import addition from "./addition.js"
+addition();
