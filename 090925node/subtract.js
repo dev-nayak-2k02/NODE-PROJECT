fcfeb2ef5f7f602c2a93f50c700db196105cfb75
dev nayak10 <devprasannanayak@gmail.com>
2025-09-10 23:17:36 +0530

    content added

---
+++ 090925node/subtract.js
@@ -0,0 +1,9 @@
+function subtraction(){
+    var a = 50;
+    var b = 70;
+    var s = a+b;
+    console.log('First number: ',a);
+    console.log('Second number: ',b);
+    console.log('Sum is: ',s);
+}
+export default subtraction;
