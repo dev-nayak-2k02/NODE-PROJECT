f28c630bbb3717b64d25d24092a60c009e7bd603
dev nayak10 <devprasannanayak@gmail.com>
2025-10-03 20:24:58 +0530

    content added

---
+++ 270925node/addition.js
@@ -0,0 +1,10 @@
+function addition(){
+    let a = 10;
+    let b = 20;
+    let s = a+b;
+    console.log(`first number: ${a}`);
+    console.log(`second number: ${b}`);
+    console.log(`SUM: ${s}`);
+}
+addition()
+// module.exports = addition;
