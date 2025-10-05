9a27354b1b4be6f95559f9fd87eb0757d18c9673
dev nayak10 <devprasannanayak@gmail.com>
2025-10-05 17:20:46 +0530

    content added

--- 051025node/index.js
+++ 051025node/index.js
@@ -6,6 +6,13 @@ app.post('/search/:id',(req,res)=>{
     res.send('something happened')
 })
 
+app.get('/register',(req,res)=>{
+    console.log(req.query)
+    console.log(req.query.roll)
+    res.send('something happened')
+    Number(req.query.roll) == 101? res.send(`yes it is`) : res.send(`no it is not`)
+})
+
 const PORT = 8000;
 
 app.listen(PORT,()=>{
