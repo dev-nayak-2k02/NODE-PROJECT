f855b48e3b09b0039dfe9912f823737df4aa536c
dev nayak10 <devprasannanayak@gmail.com>
2025-10-05 17:10:34 +0530

    content added

--- 051025node/index.js
+++ 051025node/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.post('/search/:id',(req,res)=>{
-    console.log(req.id)
+    console.log(req.params.id)
     res.send('something happened')
 })
 
