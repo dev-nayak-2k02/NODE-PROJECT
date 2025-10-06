39f6c9f72a84d40eec4d1b6f0be377e7602b8f6e
dev nayak10 <devprasannanayak@gmail.com>
2025-10-06 10:14:23 +0530

    content added

--- 061025node/index.js
+++ 061025node/index.js
@@ -1,6 +1,34 @@
 const express = require('express')
 const app = express()
 
+app.get('/about',(req, res)=>{
+    const product = [
+        {
+            "name": "Iphone 17 Pro(12GB RAM, 256GB Storage)",
+            "brand": "Apple",
+            "color": "Punk Orange",
+            "processor": "A-19 Bionic chip",
+            "price": '1,30,000'
+        },
+        {
+            "name": "Galaxy S25 Pro-max(12GB RAM, 256GB Storage)",
+            "brand": "Samsung",
+            "color": "Space Grey",
+            "processor": "Snapdragon-8 Elite",
+            "price": '1,17,000'
+        },
+        {
+            "name": "IQOO Neo 10(8GB RAM, 256GB Storage)",
+            "brand": "IQOO(Vivo)",
+            "color": "Platinum Chrome",
+            "processor": "Snapdragon-8s Gen 4",
+            "price": "30,000"
+        }
+    ]
+    res.send(product)
+    console.log(product);
+})
+
 const PORT = 8000;
 
 app.listen(PORT,()=>{
